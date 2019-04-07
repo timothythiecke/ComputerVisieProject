df9ef2793bc8f7bb5f7197495edd40db857e75a2
monoscale <bertdesaffel@gmail.com>
2019-04-07 02:27:19 +0200

    try to detect lines: added trackbars to change parameters more easily

--- Modules/PaintingDetector.py
+++ Modules/PaintingDetector.py
@@ -4,9 +4,9 @@ from Modules import imgproc, colors
 
 class PaintingDetector():
     @classmethod
-    def detectPainting(self, image):
+    def detectPainting(self, image, lowThreshold, ratio):
         # canny recommends an upper:lower ratio between 2:1 and 3:1
-        image = self._detectLines(image=image, lowThreshold=100, ratio=2, rho=1, theta=numpy.pi/180)
+        image = self._detectLines(image=image, lowThreshold=lowThreshold, ratio=ratio, rho=1, theta=numpy.pi/180)
         return image
 
     @classmethod
@@ -16,15 +16,18 @@ class PaintingDetector():
         edges = cv2.Canny(image=image,
                           threshold1=lowThreshold,
                           threshold2=lowThreshold * ratio,
+                          apertureSize=3,
                           L2gradient=True)
         lines = cv2.HoughLinesP(image=edges,
                                 rho=rho, theta=theta,
-                                threshold=30, maxLineGap=10)
-        for line in lines:
-            print(line)
-            cv2.line(img=result,
-                     pt1=(line[0][0], line[0][1]), pt2=(line[0][2], line[0][3]),
-                     color=colors.GREEN, thickness=2, lineType=cv2.LINE_AA)    
+                                minLineLength=100,
+                                threshold=50, maxLineGap=10)
+        if lines is not None:
+            for line in lines:
+                cv2.line(img=result,
+                        pt1=(line[0][0], line[0][1]), pt2=(line[0][2], line[0][3]),
+                        color=colors.GREEN, thickness=2, lineType=cv2.LINE_AA)
+            
         return result
 
     @classmethod
@@ -33,3 +36,4 @@ class PaintingDetector():
         edges = cv2.Canny(image=image, threshold1=lowThreshold, threshold2=lowThreshold * ratio)
         contours, hierarchy = cv2.findContours(image=edges, mode=cv2.RETR_TREE , method=cv2.CHAIN_APPROX_SIMPLE)
         cv2.drawContours(image=image, contours=contours, contourIdx=-1, color=colors.GREEN, thickness = 2)
+
--- main.py
+++ main.py
@@ -5,18 +5,29 @@ import screeninfo
 from Modules import optcheck, highgui, imgproc
 from Modules import PaintingDetector as pd
 
+
 def main():
     imagePath = optcheck.getArguments()[0]
     image = highgui.openImage(imagePath)
-
-    monitor = screeninfo.get_monitors()[0]
-    image = highgui.resizeImage(image, (monitor.width >> 1, monitor.height >> 1)) # adjust width and height to base screen
-    
-    paintingDetector = pd.PaintingDetector()
-    image = paintingDetector.detectPainting(image)
-
-    highgui.showImage("name",image)
- 
+    original = image.copy()
+    cv2.namedWindow(imagePath)
+    # this function excepts a reference to a function. We actaully want to do nothing
+    # using the keyword 'pass' does not work so we just 'add' a number to the trackbarPosition, which does nothing
+    cv2.createTrackbar('lowThreshold', imagePath, 50, 500, lambda x: x + 1) # 
+    cv2.createTrackbar('ratio', imagePath, 2, 5, lambda x: x + 1)
+    while True:    
+        lowThreshold = cv2.getTrackbarPos('lowThreshold', imagePath)
+        ratio = cv2.getTrackbarPos('ratio', imagePath)
+        paintingDetector = pd.PaintingDetector()
+        image = paintingDetector.detectPainting(original, lowThreshold, ratio)
+        monitor = screeninfo.get_monitors()[0]
+        # do not resize the image before detecting the lines. A different resolution produces a different output.
+        image = highgui.resizeImage(image, (monitor.width >> 1, monitor.height >> 1)) # adjust width and height to base screen
+        
+        cv2.imshow(imagePath, image)
+        ch = cv2.waitKey()
+        if(ch == 27): # escape key
+            break
 
 if __name__ == '__main__':
     main()
