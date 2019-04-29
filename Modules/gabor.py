8a49513dbe8135b0154d8f2f94578adb3b75b8bc
timothythiecke <timothy.thiecke@ugent.be>
2019-04-30 00:53:09 +0200

    Cleaned up gabor module a little bit, added additional commentary and todos

--- Modules/gabor.py
+++ Modules/gabor.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
+from Modules import colors
 
-def segmentImage(image):
+def segmentImage(image, debug = False):
     """
     Segments image in three by applying gabor filters
     Parameters
@@ -13,23 +14,13 @@ def segmentImage(image):
         output
             Image segmented in three colors
     """
-
-
-######
-# 2 DATA met meerdere features
-######
-# Data in vorige was 50 elementen in z, met elk een array
-# Nu 50 elementen met elk array van 2 elementen
-# Eerste kolom komt overeen met feature 1
-# Eerste kolom komt overeen met feature 2 #in geval van gabor filter komt dit overeen met de response waarde
-
-    
-    #cv2.imshow('image', resized)
-    #print(resized.shape)
+    # Additional info
     # https://www.mathworks.com/help/images/texture-segmentation-using-gabor-filters.html
     # https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_ml/py_kmeans/py_kmeans_opencv/py_kmeans_opencv.html
     # http://www.pami.uwaterloo.ca/pub/hammouda/sd775-paper.pdf # Read this paper
 
+    # Resize image to a smaller format
+    # TODO: should be a function argument?
     resized = np.zeros((0, 0))
     resize_scale = 0.25
     resized = cv2.resize(image, (0,0), resized, resize_scale, resize_scale)
@@ -38,15 +29,14 @@ def segmentImage(image):
     grayscale = np.zeros((0, 0))
     grayscale = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
     
-    cv2.imshow('grayscale', grayscale)
-
+    cv2.imshow('gabor_grayscale', grayscale)
     
-    # Init
-    sigmas =  [ 0.5, 1.5, 2.5]#, 3.5, 4.5, 5.0] #[7.0, 8.0, 9.0, 10.0, 11.0, 12.0] #[ 0.5, 1.5, 2.5, 3.5]
-    thetas = [0.0, 1/6, 1/3, 0.5, 2/3, 5/6] #, 1.0]#, 1.25, 1.5, 1.75] # vanaf 1.0 pi is de kernel volledig symmetrisch met 0
+    # Init of filter bank parameters
+    # TODO: these values need to be tweaked as they do not yield a perfect result yet
+    sigmas =  [ 0.5, 1.5, 2.5]
+    thetas = [0.0, 1/6, 1/3, 0.5, 2/3, 5/6]
     lambdas = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
-    #lambdas = [3.0]
-    psis = [0.0]#, 1/6, 1/3, 0.5, 2/3, 5/6]
+    psis = [0.0]
     gamma = 1.0 
     kernel_size = 17
 
@@ -65,8 +55,11 @@ def segmentImage(image):
                     i += 1
 
     # Create data set for kmeans
-    # True -> Adds X, Y coordinate to feature vector
-    # True -> Adds RGB color of pixel to feature vector
+    # Rows determined by amount of pixels in picture
+    # Dataset will be Nx(M+O+P) where N is the amount of pixels, M the amount of filters in the bank,
+    # O two additional columns for coordinate of pixel and P three additional columns for RGB color value
+    # True/False -> Adds/omits X, Y coordinate to feature vector
+    # True/False -> Adds/omits RGB color of pixel to feature vector
     use_position = True 
     use_RGB = False 
 
@@ -76,8 +69,9 @@ def segmentImage(image):
     if use_RGB:
         data_set_rows += 3
 
-    data_set = np.zeros((resized.shape[0] * resized.shape[1], data_set_rows)) # Rows determined by amount of pixels in picture
-    print(data_set.shape)
+    data_set = np.zeros((resized.shape[0] * resized.shape[1], data_set_rows))
+    if debug:
+        print('Dataset shape: ', data_set.shape)
 
     if use_position or use_RGB:
         for i in range(resized.shape[0]):
@@ -85,18 +79,6 @@ def segmentImage(image):
                 one_d_index = (i * resized.shape[1]) + j
                 col = gabor_iterations
                 if use_position: # A possible heuristic might be to have the pixels in the middle have a heavier average
-                    #alpha = j / resized.shape[1]
-                    #if alpha < 0.5:
-                    #    data_set[one_d_index, col] = alpha
-                    #elif alpha > 0.5:
-                    #    data_set[one_d_index, col] = 1.0 - alpha
-                    #col += 1
-                    #alpha = i / resized.shape[0]
-                    #if alpha < 0.5:
-                    #    data_set[one_d_index, col] = alpha
-                    #elif alpha > 0.5:
-                    #    data_set[one_d_index, col] = 1.0 - alpha
-                    #col += 1
                     data_set[one_d_index, col] = j
                     col += 1
                     data_set[one_d_index, col] = i
@@ -110,9 +92,8 @@ def segmentImage(image):
                     data_set[one_d_index, col] = resized[i, j][2]
                     col += 1
 
-    #data_set[2][25] = 1337
-    #data_set[2, 25] = 1337
-    #print(data_set[2])
+    if debug:
+        print('Third row of data set: ', data_set[2])
 
     # Apply gabor filters and fill up data_set
     for i in range(gabor_iterations):
@@ -126,18 +107,16 @@ def segmentImage(image):
         gaussian = cv2.GaussianBlur(src = filter_bank[i], ksize = (11, 11), sigmaX = sigma, sigmaY = sigma)
 
         response = np.zeros((0, 0))
-        response = cv2.filter2D(src = grayscale, ddepth = cv2.CV_64F, kernel = np.power(gaussian, 2), dst = response)
-        #cv2.imshow('filtered' + str(i), response)
-        #print(response)
+        #response = cv2.filter2D(src = grayscale, ddepth = cv2.CV_64F, kernel = np.power(gaussian, 2), dst = response)
+        response = cv2.filter2D(src = grayscale, ddepth = cv2.CV_64F, kernel = gaussian, dst = response)
 
+        # Visualize response
         normalized = np.zeros((0,0))
         normalized = cv2.normalize(src = response, dst = normalized, alpha = 255.0, beta = 0.0, norm_type = cv2.NORM_MINMAX)
-        #cv2.imshow('normalized' + str(i), normalized.astype(np.uint8)) # Hierbij gaat enorm veel precizie verloren (enkel nuttig om te visualizeren)
+        #cv2.imshow('normalized' + str(i), normalized.astype(np.uint8)) # A lot of precision is lost, it is only useful for visualization purposes
 
-        #print(normalized)
-        #print(normalized.astype(np.uint8))
-
-        minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(src = normalized)
+        # Determine min and max value (currently only debug)
+        #minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(src = normalized)
         #print(minVal, maxVal)
 
         # Then, per pixel, store in dataset
@@ -146,16 +125,13 @@ def segmentImage(image):
         #print(response.flatten())
         data_set[:, i] = np.array(response.flatten())
 
-        #TODO: iterate over pixels, store position and RGB value in data_set as last columns
-
 
     '''# Each pixel now has a feature vector associated with it
     # We could segment the image into smaller blocks (like lab 6) to reduce the amount of feature vectors
     # 16 x 16?, requires image to be scaled into something divisble by 16
     # Append x,y information of block/pixel to feature vector'''
 
-    # Use the magnitude of the feature vectors
-    #print(np.linalg.norm(data_set[2][0:gabor_iterations]))
+    # Use the magnitude of the feature vectors (responses of gabor filters)
     columns = 1
     if use_position:
         columns += 2
@@ -164,8 +140,11 @@ def segmentImage(image):
     data_set_magnitude = np.zeros(((resized.shape[0] * resized.shape[1], columns)))
 
     # Copy from first dataset to new data set
+    # TODO: this part can be reduced to a more simple python numpy assignment
     for i in range(resized.shape[0] * resized.shape[1]):
-        data_set_magnitude[i][0] = np.linalg.norm(data_set[i][0:gabor_iterations])
+        #data_set_magnitude[i][0] = np.linalg.norm(data_set[i][0:gabor_iterations])
+        v = data_set[i][0:gabor_iterations]
+        data_set_magnitude[i][0] = np.sqrt(v.dot(v))
         col = 1
         if use_position:
             data_set_magnitude[i][col] = data_set[i][gabor_iterations + col - 1]
@@ -180,21 +159,26 @@ def segmentImage(image):
             data_set_magnitude[i][col] = data_set[i][gabor_iterations + col - 1]
             col += 1
 
-    print(data_set_magnitude)
+    if debug:
+        print(data_set_magnitude)
 
     # Apply kmeans clustering
     # Define criteria = ( type, max_iter = 10 , epsilon = 1.0 )
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 
-    # Set flags (Just to avoid line break in the code)
+    # Set flags
     flags = cv2.KMEANS_PP_CENTERS 
 
+    # Check precision loss
     '''print(data_set[2])
     print(np.float32(data_set)) # A little bit of precision is lost'''
 
     # Apply KMeans
-    compactness,labels,centers = cv2.kmeans(np.float32(data_set_magnitude), 3, None,criteria, 10, flags) #Kmeans expects float32 TODO, does it change the values?
-    #print(labels)
+    clusters = 3
+    compactness,labels,centers = cv2.kmeans(np.float32(data_set_magnitude), clusters, None,criteria, 10, flags) #Kmeans expects float32 TODO, does it change the values? see above
+    
+    if debug:
+        print('Labels array: ', labels)
 
     # The elements of the labels array define which class at a certain element the pixel belongs to
     # Based on this we can draw an image
@@ -203,14 +187,14 @@ def segmentImage(image):
         for j in range(resized.shape[1]):
             one_d_index = (i * resized.shape[1]) + j
             if labels[one_d_index] == 0:
-                output[i, j] = (200, 0, 0)
+                output[i, j] = colors.GREEN
             if labels[one_d_index] == 1:
-                output[i, j] = (0, 200, 0)
+                output[i, j] = colors.RED
             if labels[one_d_index] == 2:
-                output[i, j] = (0, 0, 200)
+                output[i, j] = colors.BLUE
             if labels[one_d_index] == 3:
-                output[i, j] = (0, 200, 200)
+                output[i, j] = colors.CYAN
             if labels[one_d_index] == 4:
-                output[i, j] = (200, 0, 200)
+                output[i, j] = colors.ORANGE
 
     return output
