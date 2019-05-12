c16516509536a5fa1b46ee973fcd898406383ccc
timothythiecke <timothy.thiecke@ugent.be>
2019-05-12 22:30:27 +0200

    Rewrote part of the matching module so it looks for the top n matches in the database

--- Modules/matching.py
+++ Modules/matching.py
@@ -7,48 +7,64 @@ from Modules import optcheck, highgui, imgproc, contour
 from Modules import PaintingDetector as pd
 from matplotlib import pyplot as plt
 
-def match():
-    #TODO: Temp code
-
-    # Load dataset (training)
-    data_set = []
-    directory = os.fsencode('./Images/DataSet/Zaal_B/')
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        image = highgui.openImage('./Images/DataSet/Zaal_B/' + filename)
-        image = cv2.resize(src = image, dsize = (0, 0), dst = None, fx = 0.125, fy = 0.125)
-        gray = cv2.cvtColor(src = image, code = cv2.COLOR_BGR2GRAY)
-        data_set.append(gray)
-        
-        # TODO: Good features to track veranderen
-        #corners = cv2.goodFeaturesToTrack(image = gray, maxCorners = 20, qualityLevel = 0.01, minDistance = 10)
-        #for corner in corners:
-        #    cv2.circle(image, (corner[0][0], corner[0][1]), 4, (0, 255, 0), 2)    
-
-        #cv2.imshow(filename, image)
-        #contour.contour(image, './Images/Zaal_A/' + filename)
+def match(image, dataSet, topMatchesCount = 5, debug = False):
+    """
+    Tries to match the given image with an image in the dataset.
+    The function shows the 5 most likely matches.
+    Parameters
+    ----------
+        image 
+            Image in standard format to match with dataset
+        dataSet
+            THE dataset
+        topMatchesCount
+            The top amount of matches to show
+        debug
+            If set to true, the program will print additional information and open windows
+    """
     
-
-    # Load query image and contour it
-    imagePath = optcheck.getArguments()[0]
-    image = highgui.openImage(imagePath)
-    result = contour.contour(image, 'imagepath', True)
-
-
     ### Matching stage ###
     # Initiate SIFT detector
     orb = cv2.ORB_create()
     #sift = cv2.SIFT()
-    kp_result, desc_result = orb.detectAndCompute(result, None)
+    kp_result, desc_result = orb.detectAndCompute(image, None)
 
     # Create BFMatcher
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
 
     i = 0
-    for data_set_image in data_set:
+    results = []
+    for data_set_tuple in dataSet:
+        data_set_image = data_set_tuple[0]
+        data_set_meta_data = data_set_tuple[1]
         kp_d, desc_d = orb.detectAndCompute(data_set_image, None)
         matches = bf.match(desc_result, desc_d)
         matches = sorted(matches, key = lambda x:x.distance)
-        comparison = cv2.drawMatches(img1 = result, keypoints1 = kp_result, img2 = data_set_image, keypoints2 = kp_d, matches1to2 = matches[:20], outImg = None, flags = 2)
-        cv2.imshow(str(i), comparison)
+
+        matches_to_use = 20
+        matches = matches[:matches_to_use]        
+        distance_sum = sum(match.distance for match in matches)
+
+        if debug:
+            print('\tPainting', i, 'Sum of matches:', distance_sum)
+            #for m in matches:
+            #    print('\t', m.distance)
+
+        comparison = cv2.drawMatches(img1 = image, keypoints1 = kp_result, img2 = data_set_image, keypoints2 = kp_d, matches1to2 = matches[:matches_to_use], outImg = None, flags = 2)
+        results.append((i, distance_sum, comparison))    
         i += 1
+
+    # Distance is the notion of similiarity
+    # The lower the distance, the mr=ore similar detected keypoints are
+    # Therefore, the lower the sum of detected mathes, the higher the odds that it is a valid match
+    # Sort the results array by the sum associated with the picture
+    results = sorted(results, key = lambda x:x[1])
+
+    # Show the best results
+    for i in range(topMatchesCount):
+        if i == 0:
+            cv2.imshow('Best match ->' + str(results[i][0]), results[i][2])
+            print('You are located in', dataSet[results[i][0]][1])
+        else:
+            cv2.imshow(str(results[i][0]), results[i][2])
+        print('Painting', results[i][0], 'Sum: of matches', results[i][1])
