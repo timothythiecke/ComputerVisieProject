441e224e12695acad0a84c24457547a57d843bd5
Timothy Thiecke <timothy.thiecke@gmail.com>
2019-05-21 23:02:26 +0200

    Code for video

--- main.py
+++ main.py
@@ -12,5 +12,42 @@ def main():
     groundPlan.markVisited('5')
     groundPlan.visualize()
 
+    # Video code
+    videoPath = optcheck.getArguments()[0]
+
+    # Get the dataset from disk
+    data_set = dataset.get(resetPersistence = False)
+    
+    # Video code
+    capture = cv2.VideoCapture(videoPath) # Use command line argument as above
+    i = 0
+    frame_index = 0
+    thread_i = 0
+
+    groundPlan = GroundPlan.GroundPlan()
+
+    executor = ThreadPoolExecutor(max_workers=32)
+    while (capture.isOpened()):
+        ret, frame = capture.read()
+
+        extracted = contour.contour(image = frame, scale = 0.5, imagepath = 'extracted', showExtracted = True)
+        if i == 30: # Every 30 frames, a new matching procedure is started, we let the executor handle threading for us
+            f = executor.submit(matching.match, np.copy(extracted), data_set, 1, False, frame_index, groundPlan)
+        
+        i += 1
+        frame_index += 1
+        frame = contour.drawOnVideoFrame(frame = frame, scale = 0.5)     
+
+        cv2.imshow('movie', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    capture.release()
+
+    groundPlan.visualize()    
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
 if __name__ == '__main__':
     main()
