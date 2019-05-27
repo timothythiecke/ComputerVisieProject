08f0cb70f2be8f9b75112f917f8c2d330edef533
timothythiecke <timothy.thiecke@ugent.be>
2019-05-27 18:37:19 +0200

    Reverted some changes

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import screeninfo
 import numpy
 import cv2
 import numpy as np
-from Modules.Dataset import DataSet
-from Modules.GroundPlan import GroundPlan
-from Modules.Contour import contour, drawOnVideoFrame
+import queue
+import threading
+from Modules.dataset import DataSet
+from Modules.GroundPlan import GroundPlan, groundPlanMessageConsumer
+from Modules.contour import contour, drawOnVideoFrame
 from concurrent.futures import ThreadPoolExecutor
 from Modules import optcheck, highgui, imgproc, matching
 
@@ -15,21 +17,28 @@ def main():
     #paintingFinder = PaintingFinder()
     capture = cv2.VideoCapture(videoPath)
     executor = ThreadPoolExecutor(max_workers=32)
+    
+    frame_interval = 30
+
+    # Spin up a thread that consumes the results of the matching procedures in different threads
+    q = queue.Queue()
+    groundPlanConsumer = threading.Thread(target = groundPlanMessageConsumer, args=[groundPlan, q])
+    groundPlanConsumer.start()
 
     (frameCount, frameIndex) = [0] * 2 # initialize both values to zero
-    
+
     while(capture.isOpened()):
         frame = capture.read()[1] # discard the 'succeeded' variable
         extracted = contour(image = frame, scale = 0.5, imagepath = 'extracted', showExtracted = True)
-        if frameCount == 30:
-            executor.submit(matching.match, np.copy(extracted), dataSet, 1, False, frameIndex, groundPlan, False)
+        if frameCount == frame_interval:
+            executor.submit(matching.match, np.copy(extracted), dataSet, 1, frameIndex, q, False)
             frameCount = 0
 
         frameCount += 1
         frameIndex += 1
         frame = drawOnVideoFrame(frame = frame, scale = 0.5)
         cv2.imshow("movie", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
     capture.release()
     q.put('q') # in order to stop the groundplan consumer, threads can't be stopped externally
